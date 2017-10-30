1f4fb96612eda1deaedc1f654f90eb304ce9186d
Camden Ko <camdenko@gmail.com>
2017-10-30 16:56:53 -0500

    is youtube

--- playlist_gen.py
+++ playlist_gen.py
@@ -1,21 +1,33 @@
 import praw
+import re
 import config
 
 def bot_login():
   return praw.Reddit(
-    username = config.username,
-    password = config.password,
-    client_id = config.client_id,
-    client_secret = config.client_secret,
-    user_agent = "playlistGenv1.0"
+      username=config.username,
+      password=config.password,
+      client_id=config.client_id,
+      client_secret=config.client_secret,
+      user_agent="playlistGenv1.0"
   )
+
+def is_youtube(url):
+  youtube_regex = re.compile(
+      r'(https?://)?(www\.)?'
+      r'(youtube|youtu|youtube-nocookie)\.(com|be)/'
+      r'(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+  return not not youtube_regex.match(url)
 
 def is_valid_post(post):
   return not post.stickied
 
-def run_bot(reddit):
-  for submission in reddit.subreddit('listentothis').hot(limit = 3):
-    print (is_valid_post(submission))
+def get_posts(reddit, subreddit="listentothis"):
+  posts = []
+  for submission in reddit.subreddit(subreddit).hot():
+    if is_valid_post(submission):
+      posts.append(submission.url)
+  print is_youtube(posts[0])
+  return posts[:25]
 
-reddit = bot_login()
-run_bot(reddit = reddit)
+REDDIT = bot_login()
+# get_posts(reddit=REDDIT)
