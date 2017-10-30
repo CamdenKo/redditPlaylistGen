c35858200d29c48ddd10595caa861e6900d66017
Camden Ko <camdenko@gmail.com>
2017-10-30 16:15:13 -0500

    now prints out top result

--- playlist_gen.py
+++ playlist_gen.py
@@ -1,3 +1,21 @@
 import praw
-import sys
-print ('\n'.join(sys.path))
+import config
+
+def bot_login():
+  return praw.Reddit(
+    username = config.username,
+    password = config.password,
+    client_id = config.client_id,
+    client_secret = config.client_secret,
+    user_agent = "playlistGenv1.0"
+  )
+
+def is_valid_post(post):
+  return not post.stickied
+
+def run_bot(reddit):
+  for submission in reddit.subreddit('listentothis').hot(limit = 3):
+    print (is_valid_post(submission))
+
+reddit = bot_login()
+run_bot(reddit = reddit)
